c008a9446a752abda1b08267c2c41f44ff3c7926
jefferymason <52844021+jefferymason@users.noreply.github.com>
2021-01-10 10:46:42 -0600

    Add files via upload

---
+++ Bullseye/Bullseye/Bullseye/App/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Jeff Mason on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
